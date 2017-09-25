16fa46aa4d7ea3b8d2c2deecb055ceff7cec13ff
Aditya Sehgal <aditya.sehgal@inmarsat.com>
2017-09-25 16:48:13 +0100

    corrected the command line options

--- convert.py
+++ convert.py
@@ -5,7 +5,8 @@ import argparse
 
 def convert():
     parser  = argparse.ArgumentParser()
-    parser.add_argument("image_path", help="Path of the images")
+    parser.add_argument("xml_path", help="directory where the annotation files are present")
+    parser.add_argument("--image_path", help="Path of the images. If this is provided,path within the label will be changed to the provided option")
     parser.add_argument("--output_file", help="Name of the output file. If not provided, assumed to be traininfile.txt")
     parser.add_argument("--append", help="true if you want to append the results to the output file. Assumed false")
     args = parser.parse_args()
@@ -19,18 +20,25 @@ def convert():
        mode = "a+"
         
     handle = open(outputFile,mode)
+    path = args.xml_path + "/*.xml"
+    print path
 
-   
-    for filename in glob.iglob('*.xml'):
+    for filename in glob.iglob(path):
+      print filename 
       root = xml.etree.ElementTree.parse(filename).getroot()
+      if args.image_path:
+         image_path = args.image_path + '/' + filename 
+      else:
+         image_path = root.find('path').text
+
       for object in root.findall('object'):
-        textToWrite = args.image_path + '/' + filename 
         className = object.find('name').text
         xmin = object.find('bndbox').find('xmin').text
         ymin = object.find('bndbox').find('ymin').text
         xmax = object.find('bndbox').find('xmax').text
         ymax = object.find('bndbox').find('ymax').text
-        textToWrite +=',' + xmin + ',' + ymin + ',' + xmax + ',' + ymax + ',' + className + '\n'
+        textToWrite = image_path + ',' + xmin + ',' + ymin + ',' + xmax + ',' + ymax + ',' + className + '\n'
+        print textToWrite
         handle.write(textToWrite) 
    
     print "Conversion available in " + outputFile
