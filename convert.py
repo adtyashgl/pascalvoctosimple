5f3f581f7755f344543ae2bffd63bc3c3aa46543
Aditya Sehgal <aditya.sehgal@inmarsat.com>
2017-09-25 15:58:47 +0100

    added support for command line options

--- convert.py
+++ convert.py
@@ -1,18 +1,35 @@
 import xml.etree.ElementTree
 import glob 
-IMAGE_PATH='/home/ubuntu/keras-frcnn/training_13_06/images'
-SIMPLE_TRAINING_FILE = 'trainingfile.txt'
+import argparse
 
-f=open(SIMPLE_TRAINING_FILE,"w+")
 
-for filename in glob.iglob('*.xml'):
-  root = xml.etree.ElementTree.parse(filename).getroot()
-  for object in root.findall('object'):
-     textToWrite = IMAGE_PATH + '/' + filename 
-     className = object.find('name').text
-     xmin = object.find('bndbox').find('xmin').text
-     ymin = object.find('bndbox').find('ymin').text
-     xmax = object.find('bndbox').find('xmax').text
-     ymax = object.find('bndbox').find('ymax').text
-     textToWrite +=',' + xmin + ',' + ymin + ',' + xmax + ',' + ymax + ',' + className + '\n'
-     f.write(textToWrite) 
+def convert():
+    parser  = argparse.ArgumentParser()
+    parser.add_argument("image_path", help="Path of the images")
+    parser.add_argument("--output_file", help="Name of the output file. If not provided, assumed to be traininfile.txt")
+    args = parser.parse_args()
+  
+    outputFile = "trainingfile.txt"
+    if args.output_file:
+      outputFile = args.output_file 
+    
+    handle = open(outputFile,"w+")
+
+   
+    for filename in glob.iglob('*.xml'):
+      root = xml.etree.ElementTree.parse(filename).getroot()
+      for object in root.findall('object'):
+        textToWrite = args.image_path + '/' + filename 
+        className = object.find('name').text
+        xmin = object.find('bndbox').find('xmin').text
+        ymin = object.find('bndbox').find('ymin').text
+        xmax = object.find('bndbox').find('xmax').text
+        ymax = object.find('bndbox').find('ymax').text
+        textToWrite +=',' + xmin + ',' + ymin + ',' + xmax + ',' + ymax + ',' + className + '\n'
+        handle.write(textToWrite) 
+   
+    print "Conversion available in " + outputFile
+    handle.close()
+
+if __name__ == "__main__":
+   convert() 
